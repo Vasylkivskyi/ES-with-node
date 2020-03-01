f6a59da9267e6af75ab65eafef287fd3e36cbbce
pavlo-vasylkivskyi-scx <pavlo.vasylkivskyi@scelex.com>
2020-03-02 00:52:44 +0200

    init commit

---
+++ es-backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Works 🤗');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT} 🤝`);
+});
