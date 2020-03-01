ea5969d412060eaa96932e52d8a536ea3949c891
Pavel Vasylkivskiy <pavelvasylkivskiy@MacBook-Pro.local>
2020-03-02 01:25:57 +0200

    test

--- es-backend/index.js
+++ es-backend/index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`App is running on port ${PORT} 🤝`);
+  console.log(`App is running on port ${PORT}...`);
 });
