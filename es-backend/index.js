6808a7bedb0f5c208b7305f6245fdef588bbf64e
Pavel Vasylkivskiy <pavel.vasylkivskiy@gmail.com>
2020-03-02 01:50:47 +0200

    test6

--- es-backend/index.js
+++ es-backend/index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}...`);
+  console.log(`App is running on port ${PORT}...👻`);
 });
